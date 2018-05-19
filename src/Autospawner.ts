9da9ae77531a74460f80fd09c2a5c9884b2f71a8
Aerue\Alexandre <contact@alexandre-perrigault.fr>
2018-05-19 21:45:00 +0200

    ...

--- src/Autospawner.ts
+++ src/Autospawner.ts
@@ -5,19 +5,36 @@ class Autospawner {
   protected _api: any = null;
   protected _user: object = {};
 
+  protected _worldStatus: string = "";
+
   constructor() {
     const config = require("../screeps.json");
-    this._api = new ScreepsAPI(config.wepapi);
-    this._api.me().then((user: object) =>
-      this._user = user
-    );
 
-    this._api.
+    this._api = new ScreepsAPI(config.wepapi);
+
+    // this._api.me()
+    //   .then((user: object) =>
+    //     this._user = user
+    //     // TODO : Make promise here ...
+    //   );
+
+    this._getWorldStatus();
 
   }
 
   protected _getShard(): void {
     const test = this._user;
+  }
+
+  protected _getWorldStatus(): string {
+    if (this._worldStatus !== "") {
+      return this._worldStatus;
+    }
+    this._api.raw.user.worldStatus()
+      .then((data: any) => {
+        this._worldStatus = data.status;
+      });
+    return this._worldStatus;
   }
 
 }
